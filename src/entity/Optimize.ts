3a9f995ecaddee87e1583049a7641fdd29ba77aa
김승우 <tmd8635@gmail.com>
2025-05-17 00:58:20 +0900

    크롤링 최적화

---
+++ src/entity/Optimize.ts
@@ -0,0 +1,27 @@
+
+
+export class Optimizer {
+
+    private readonly optimizes: Set<string> = new Set();
+
+    register(optimizer: Optimize) {
+        this.optimizes.add(optimizer)
+    }
+    registerAll(optimizes : Optimize[]) {
+        optimizes.forEach(optimize => this.register(optimize));
+    }
+
+    isBlocked(resourceType: string) : boolean {
+        return this.optimizes.has(resourceType)
+    }
+
+
+}
+export enum Optimize {
+
+    CSS = 'stylesheet',  // CSS 파일들
+    JS = 'script',      // JavaScript 파일
+    IMAGE = 'image',       // PNG, JPG, GIF, SVG 등
+    FONT = 'font',        // WOFF, WOFF2, TTF 등
+    MEDIA = 'media'       // 비디오/오디오 파일
+}
