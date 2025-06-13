7043b39369b83e567c507670e0814a791196517b
kimdonghyun <ghffu405@gmail.com>
2025-06-13 15:55:19 +0900

    test: withSyncErrorHandling 테스트 케이스 추가

--- tests/utils/ErrorHandling.test.ts
+++ tests/utils/ErrorHandling.test.ts
@@ -9,7 +9,7 @@ describe('withErrorHandling', () => {
     it('정상적인 비동기 함수의 결과값을 Result 타입으로 변환하여 반환해야 한다', async () => {
         //given
         const asyncSuccessFn = async (x: number) => x * 3;
-        const sut = withErrorHandling(asyncSuccessFn, '테스트');
+        const sut = withErrorHandling(asyncSuccessFn, 'async-test');
         //when
         const result = await sut(4);
         //then
@@ -24,14 +24,45 @@ describe('withErrorHandling', () => {
         const asyncErrorFn = async () => {
             throw new Error('async error');
         };
-        const sut = withErrorHandling(asyncErrorFn, 'async-test');
+        const sut = withErrorHandling(asyncErrorFn, 'async-error-test');
         //when
         const result = await sut();
         //then
         expect(result).toEqual({
             success: false,
             error: new Error('async error'),
-            context: 'async-test',
+            context: 'async-error-test',
+        });
+    });
+});
+
+describe('withSyncErrorHandling', () => {
+    it('정상적인 동기 함수의 결과값을 Result 타입으로 변환하여 반환해야 한다', () => {
+        //given
+        const syncSuccessFn = (x: number) => x * 3;
+        const sut = withSyncErrorHandling(syncSuccessFn, 'sync-test');
+        //when
+        const result = sut(4);
+        //then
+        expect(result).toEqual({
+            success: true,
+            data: 12,
+        });
+    });
+
+    it('동기 함수에서 발생한 에러를 Result 타입으로 변환하여 반환해야 한다', () => {
+        //given
+        const syncErrorFn = () => {
+            throw new Error('sync error');
+        };
+        const sut = withSyncErrorHandling(syncErrorFn, 'sync-error-test');
+        //when
+        const result = sut();
+        //then
+        expect(result).toEqual({
+            success: false,
+            error: new Error('sync error'),
+            context: 'sync-error-test',
         });
     });
 });
