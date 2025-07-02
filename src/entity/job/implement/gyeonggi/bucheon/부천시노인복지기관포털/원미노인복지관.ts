bdac166190a51489be71f42082771e743be1dd7c
kimdonghyun <ghffu405@gmail.com>
2025-07-02 17:48:44 +0900

    fix:  미추홀장애인종합복지관 URL 수정

--- src/entity/job/implement/gyeonggi/bucheon/부천시노인복지기관포털/원미노인복지관.ts
+++ src/entity/job/implement/gyeonggi/bucheon/부천시노인복지기관포털/원미노인복지관.ts
@@ -6,7 +6,7 @@ import { Optimize, Optimizer } from '../../../../../Optimize';
 
 export class 원미노인복지관 extends AbstractJob {
     constructor() {
-        super('원미노인복지관', 'https://senior.bucheon4u.kr', [
+        super(' ', 'https://senior.bucheon4u.kr', [
             new OnlyBucheonDefaultStep(
                 Category.NOTICE,
                 'https://senior.bucheon4u.kr/senior/user/bbs/BD_selectBbsList.do?q_bbsCode=1005&q_domnCode=2&q_estnColumn1=0110304&q_searchTy=&q_searchVal=&q_currPage=1&q_sortName=&q_sortOrder=',
