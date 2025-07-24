6e89c9677424ec046d3205f7ba259dbaf4f48416
kimdonghyun <ghffu405@gmail.com>
2025-07-24 18:31:30 +0900

    feat: 고강종합사회복지관 크롤링 구현

--- src/entity/job/JobRegistry.ts
+++ src/entity/job/JobRegistry.ts
@@ -17,6 +17,7 @@ import { 인천광역시사회복지사협회 } from './implement/incheon/인천
 import { 미추홀장애인종합복지관 } from './implement/incheon/미추홀장애인종합복지관';
 import { 서울시사회복지사협회 } from './implement/seoul/서울시사회복지사협회';
 import { 거모종합사회복지관 } from './implement/gyeonggi/siheung/거모종합사회복지관';
+import { 고강종합사회복지관 } from './implement/gyeonggi/bucheon/고강종합사회복지관';
 
 /**
  * 모든 크롤링 Job들을 등록하고 관리하는 Registry 클래스
@@ -41,6 +42,7 @@ export class JobRegistry {
         new 미추홀장애인종합복지관(),
         new 서울시사회복지사협회(),
         new 거모종합사회복지관(),
+        new 고강종합사회복지관(),
     ];
 
     static getAllJobs(): Job[] {
---
+++ src/entity/job/implement/gyeonggi/bucheon/고강종합사회복지관.ts
@@ -0,0 +1,48 @@
+import { Locator } from "playwright-core";
+import { Category } from "../../../../Category";
+import { Optimize, Optimizer } from "../../../../Optimize";
+import { MultiCategoryTemplateStep } from "../../../../step/MultiCategoryTemplateStep";
+import { AbstractJob } from "../../../AbstractJob";
+import { parseDate } from "../../../../../utils/DateUtils";
+import { classifyCategory } from "../../../../../utils/CategoryClassifier";
+
+export class 고강종합사회복지관 extends AbstractJob {
+    constructor() {
+        super('고강종합사회복지관', 'https://gogangwc.or.kr', [new 알림마당()])
+    }
+
+    registerOptimizer(optimizer: Optimizer) {
+        optimizer.register(Optimize.JS);
+    }
+}
+
+class 알림마당 extends MultiCategoryTemplateStep {
+    constructor() {
+        super(
+            'https://gogangwc.tistory.com/category/%EC%95%8C%EB%A6%BC%EB%A7%88%EB%8B%B9',
+            '.index-item.article-item',
+            Category.NOTICE
+        );
+    }
+
+    async select(card: Locator, baseUrl: string): Promise<object> {
+        const a = card.locator('.index-item-link');
+        const link = await a.getAttribute('href');
+        const title = (await card.locator('h3').textContent()).trim();
+
+        const dateStr = (await card.locator('.digit').textContent()).trim();
+        const createdAt = parseDate(dateStr, '. ');
+        
+        return {
+            title: title,
+            createdAt: createdAt,
+            link: baseUrl + link,
+        }
+    }
+
+    categorize(data: object): Category | null {
+        const { title } = data as { title: string };
+        return classifyCategory(title);
+    }
+}
+
